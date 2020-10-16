53a9bfd166fe60ee48477750330bceab15ef5490
Nitin Bhandari <bnitin92@gmail.com>
2020-10-15 19:31:33 -0600

    Updates

---
+++ precache-manifest.8d45e23a5b0bf5e0a020f28a10daf9d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b635e07799905019ebd6c1b3d4ea90d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d70303e439a738dae0f7",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "36dc4c21ac5707915aff",
+    "url": "/static/js/2.1288cb2e.chunk.js"
+  },
+  {
+    "revision": "d70303e439a738dae0f7",
+    "url": "/static/js/main.2a0f0249.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "11a1a485a17da74a6850011fd9ce3ed6",
+    "url": "/static/media/profile.11a1a485.jpg"
+  }
+]);
